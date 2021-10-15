3ea6b3f73912f6e0190c95154c2da536da2f0f24
Jeremy Sigrist <jsigrist1@gmail.com>
2021-10-14 22:46:45 -0700

    Switch to clang compiler

--- chess_engine/create_build.sh
+++ chess_engine/create_build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-C_COMPILER="/usr/local/bin/gcc-10"
-CXX_COMPILER="/usr/local/bin/g++-10"
+C_COMPILER="/usr/local/Cellar/llvm/13.0.0_1/bin/clang"
+CXX_COMPILER="/usr/local/Cellar/llvm/13.0.0_1/bin/clang++"
 
 
 cmake -D CMAKE_C_COMPILER="$C_COMPILER" -D CMAKE_CXX_COMPILER="$CXX_COMPILER" CMakeLists.txt
