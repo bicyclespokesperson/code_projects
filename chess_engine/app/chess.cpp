d79dbbf73f43b6dce83aad1e27cdcb1e23a836ac
Jeremy Sigrist <jsigrist1@gmail.com>
2021-10-22 09:59:15 -0700

    Clean up CMakeLists.txt files and add test case from chess.com

--- chess_engine/app/chess.cpp
+++ chess_engine/app/chess.cpp
@@ -12,18 +12,6 @@
 #include "my_assert.h"
 #include "player.h"
 
-#include <fstream>
-
-std::string read_file_contents(std::string const& filename)
-{
-  std::ifstream infile{filename};
-  if (!infile)
-  {
-    MY_ASSERT(false, "File could not be opened");
-  }
-  return {std::istreambuf_iterator<char>(infile), std::istreambuf_iterator<char>()};
-}
-
 /**
  * Play the chess game.
  * @param argc
@@ -32,21 +20,8 @@ std::string read_file_contents(std::string const& filename)
  */
 int main(int /* argc */, char* /* argv*/[])
 {
-#if 0
   Game game;
   game.run();
-#endif
-
-#if 1
-
-  std::string pgn_filename{"./data/fischer_spassky.pgn"};
-
-  auto b = Board::from_pgn(read_file_contents(pgn_filename));
-  if (b)
-  {
-    b->display(std::cout);
-  }
-#endif
 
   return 0;
 }
--- chess_engine/tests/chess_engine_tests.cpp
+++ chess_engine/tests/chess_engine_tests.cpp
@@ -36,6 +36,26 @@ static const char c_fischer_spassky_result[] =
 \
     A   B   C   D   E   F   G   H  ";
 
+static const char c_sigrist_result[] = 
+" 8  R_b ___ ___ ___ ___ ___ ___ ___ \
+\
+7  P_b P_b P_b ___ ___ ___ ___ P_b \
+\
+6  ___ ___ ___ ___ B_w ___ ___ K_b \
+\
+5  ___ ___ ___ ___ ___ ___ ___ ___ \
+\
+4  ___ ___ ___ P_w ___ Q_w ___ ___ \
+\
+3  ___ ___ N_w ___ N_b ___ ___ ___ \
+\
+2  P_w P_w P_w ___ ___ ___ P_w P_w \
+\
+1  R_w ___ ___ ___ ___ ___ K_w ___ \
+\
+    A   B   C   D   E   F   G   H  \
+";
+
 TEST_CASE("A board can be constructed from a pgn file" "[board]")
 {
   std::string const pgn_filename{"./data/fischer_spassky.pgn"};
@@ -49,5 +69,33 @@ TEST_CASE("A board can be constructed from a pgn file" "[board]")
   std::stringstream out;
   board->display(out);
 
-  REQUIRE(out.str() == c_fischer_spassky_result);
+  std::string result = out.str();
+  std::string expected = c_fischer_spassky_result;
+  result.erase(std::remove_if(result.begin(), result.end(), isspace), result.end());
+  expected.erase(std::remove_if(expected.begin(), expected.end(), isspace), expected.end());
+
+  // Compare ignoring whitespace
+  REQUIRE(result == expected);
 }
+
+TEST_CASE("Another pgn file")
+{
+  std::string const pgn_filename{"./data/sigrist.pgn"};
+  auto contents = read_file_contents(pgn_filename);
+  REQUIRE(contents.has_value());
+
+  auto board = Board::from_pgn(*contents);
+  REQUIRE(board.has_value());
+
+  std::stringstream out;
+  board->display(out);
+
+  std::string result = out.str();
+  std::string expected = c_sigrist_result;
+  result.erase(std::remove_if(result.begin(), result.end(), isspace), result.end());
+  expected.erase(std::remove_if(expected.begin(), expected.end(), isspace), expected.end());
+
+  // Compare ignoring whitespace
+  REQUIRE(result == expected);
+}
+
