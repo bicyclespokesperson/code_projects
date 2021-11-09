44d1506a4ff5b22c04931c204fb69af13868b882
Jeremy Sigrist <jsigrist1@gmail.com>
2021-11-08 16:17:20 -0800

    Add default start position to perft.cpp

--- chess_engine/app/perft.cpp
+++ chess_engine/app/perft.cpp
@@ -5,25 +5,31 @@ int main(int argc, char* argv[])
 {
   if (argc != 2 && argc != 3)
   {
-    std::cerr << "Usage: perft [fen] [depth]\n";
+    std::cerr << "Usage: perft [depth] [fen]\n";
     exit(-1);
-  }
-
-  auto board = Board::from_fen(argv[1]);
-  if (!board)
-  {
-    std::cerr << "Invalid fen string\n";
   }
 
   int depth{0};
   try
   {
-    depth = std::stoi(argv[2]);
+    depth = std::stoi(argv[1]);
   }
   catch (std::invalid_argument const& err)
   {
     std::cerr << err.what();
     exit(-1);
+  }
+
+  std::string starting_position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+  if (argc == 3)
+  {
+    starting_position = argv[2];
+  }
+
+  auto board = Board::from_fen(starting_position);
+  if (!board)
+  {
+    std::cerr << "Invalid fen string\n";
   }
 
   constexpr bool print_moves = false;
