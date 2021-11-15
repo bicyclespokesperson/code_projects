a734c5ba3bf0f5f2323e65fd6bceac3b7e113675
Jeremy Sigrist <jsigrist1@gmail.com>
2021-11-15 08:02:55 -0800

    Implement mvv/lva move ordering

--- chess_engine/include/move_orderer.h
+++ chess_engine/include/move_orderer.h
@@ -15,6 +15,8 @@ public:
   void sort_moves(std::span<Move> moves, Board const& board) const;
 
 private:
+  static constexpr size_t c_piece_count = static_cast<uint8_t>(Piece::_count) - static_cast<uint8_t>(Piece::pawn);
+  static const std::array<std::array<int, c_piece_count>, c_piece_count> mvv_lva_table;
 };
 
 #endif // MOVE_ORDERER_H
--- chess_engine/src/move_orderer.cpp
+++ chess_engine/src/move_orderer.cpp
@@ -1,19 +1,44 @@
 #include "move_orderer.h"
 
+const auto Move_orderer::mvv_lva_table = []
+{
+  static constexpr std::array pieces{Piece::king, Piece::queen, Piece::rook, Piece::bishop, Piece::knight, Piece::pawn, Piece::empty};
+
+  //NOLINTNEXTLINE(cppcoreguidelines-pro-type-member-init) Initialized in this function
+  std::array<std::array<int, c_piece_count>, c_piece_count> result;
+
+  // Create a lookup table of move priorities. Move with the most valuable victim piece will be prioritied highest,
+  // and moves with the same victim will be prioritized by the least valuable attacker. Higher priorities correspond
+  // to better moves
+
+  int current_val{1};
+
+  // Start with the least valuable victim, and increment current_val as we go so 
+  // more valuable victims have higher priorities
+  for (size_t victim{c_piece_count}; victim > 0; --victim)
+  {
+    for (size_t attacker{0}; attacker < c_piece_count; ++attacker)
+    {
+      auto victim_index = static_cast<uint8_t>(pieces[victim - 1]) - static_cast<uint8_t>(Piece::pawn);
+      auto attacker_index = static_cast<uint8_t>(pieces[attacker])- static_cast<uint8_t>(Piece::pawn);
+      result[victim_index][attacker_index] = current_val;
+      ++current_val;
+    }
+  }
+
+  return result;
+}();
+
 void Move_orderer::sort_moves(std::span<Move> moves, Board const& /* board */) const
 {
-  // Sort by (victim value) - (attacker value)
-  // This isn't quite MVV/LVA; it may be worth implementing that at some point.
-  // This one penalizes any move that isn't a capture quite a bit, and prioritizes moving lower value pieces
   std::sort(moves.begin(), moves.end(),
             [](Move m1, Move m2)
             {
-              // These correspond to the Piece enum type, so they can be indexed by static_cast<uint8>(Piece)
-              constexpr static std::array piece_values{0, 0, 1, 3, 3, 5, 9, 10, 0};
-              auto const m1_value =
-                piece_values[static_cast<uint8_t>(m1.victim)] - piece_values[static_cast<uint8_t>(m1.piece)];
-              auto const m2_value =
-                piece_values[static_cast<uint8_t>(m2.victim)] - piece_values[static_cast<uint8_t>(m2.piece)];
-              return m2_value < m1_value;
+              auto const m1_victim_index{static_cast<uint8_t>(m1.victim) - static_cast<uint8_t>(Piece::pawn)};
+              auto const m1_attacker_index{static_cast<uint8_t>(m1.piece) - static_cast<uint8_t>(Piece::pawn)};
+              auto const m2_victim_index{static_cast<uint8_t>(m2.victim) - static_cast<uint8_t>(Piece::pawn)};
+              auto const m2_attacker_index{static_cast<uint8_t>(m2.piece) - static_cast<uint8_t>(Piece::pawn)};
+
+              return mvv_lva_table[m1_victim_index][m1_attacker_index] > mvv_lva_table[m2_victim_index][m2_attacker_index];
             });
 }
