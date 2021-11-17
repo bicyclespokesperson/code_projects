ab91fc1c7eaccf88f318e5f34c9d9850bb8c7104
Jeremy Sigrist <jsigrist1@gmail.com>
2021-11-17 14:45:56 -0800

    Add debug/release flags to ./create_build.sh

--- chess_engine/create_build.sh
+++ chess_engine/create_build.sh
@@ -3,8 +3,16 @@
 C_COMPILER="/usr/local/Cellar/llvm/13.0.0_1/bin/clang"
 CXX_COMPILER="/usr/local/Cellar/llvm/13.0.0_1/bin/clang++"
 
-#BUILD_TYPE="DEBUG"
-BUILD_TYPE="RELEASE"
+if [[ "$#" -ge 3 ]]; then
+  >&2 echo "Usage: $(basename $0) [-d | -r]"
+  exit 2
+fi
+
+BUILD_TYPE="DEBUG"
+if [[ "$1" == "-r" ]]; then
+  BUILD_TYPE="RELEASE"
+fi
+echo "Build type: ${BUILD_TYPE}"
 
 #cmake -GXcode CMakeLists.txt
 cmake -D CMAKE_C_COMPILER="$C_COMPILER" -D CMAKE_CXX_COMPILER="$CXX_COMPILER" -D CMAKE_BUILD_TYPE="$BUILD_TYPE" CMakeLists.txt
