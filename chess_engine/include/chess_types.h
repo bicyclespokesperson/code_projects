3c75525c612e516dda04af980b2eb54795442686
Jeremy Sigrist <jsigrist1@gmail.com>
2021-11-26 11:13:28 -0800

    Remove Compass_dir class

--- chess_engine/include/chess_types.h
+++ chess_engine/include/chess_types.h
@@ -49,56 +49,6 @@ struct Castling_rights
   bool black_can_long_castle : 1 {true};
 };
 
-class Compass_dir
-{
-public:
-  enum Value : int32_t
-  {
-    north,
-    northwest,
-    west,
-    southwest,
-    south,
-    southeast,
-    east,
-    northeast,
-    _count,
-  };
-
-  constexpr Compass_dir(Value dir) : m_value(dir)
-  {
-  }
-
-  constexpr operator Value() const
-  {
-    return m_value;
-  }
-
-  constexpr Compass_dir& operator++()
-  {
-    m_value = static_cast<Value>(static_cast<int32_t>(m_value) + 1);
-    return *this;
-  }
-
-  explicit operator bool() = delete;
-
-  // These directions count as positive on this board representation
-  constexpr bool is_positive() const
-  {
-    constexpr std::array positive_directions{Compass_dir::north, Compass_dir::east, Compass_dir::northwest,
-                                             Compass_dir::northeast};
-
-    return std::any_of(positive_directions.cbegin(), positive_directions.cend(),
-                       [&](auto dir)
-                       {
-                         return dir == this->m_value;
-                       });
-  }
-
-private:
-  Value m_value;
-};
-
 class Threefold_repetition_detector
 {
 public:
