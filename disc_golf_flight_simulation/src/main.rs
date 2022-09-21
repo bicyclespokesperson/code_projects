0001295f1b2b068216f1a754a4a85b1a3444ed89
Jeremy Sigrist <jsigrist1@gmail.com>
2022-09-20 21:55:25 -0700

    Skeleton simulate fn with struct inputs

--- disc_golf_flight_simulation/src/main.rs
+++ disc_golf_flight_simulation/src/main.rs
@@ -1,10 +1,9 @@
 // This file is the crate root
 
 // Allow unused code in the project
-#![allow(dead_code)] 
+#![allow(dead_code)]
 
 mod path_simulator;
-
 
 fn main() {
     println!("Hello, world!");
--- disc_golf_flight_simulation/src/path_simulator.rs
+++ disc_golf_flight_simulation/src/path_simulator.rs
@@ -2,7 +2,22 @@ extern crate nalgebra_glm as glm;
 
 use glm::Vec3;
 
-pub fn simulate() -> Vec<Vec3> {
-    vec![Vec3::new(1.0, 1.0, 1.2)]
+pub struct Disc {
+    speed: f64,
+    glide: f64,
+    turn: f64,
+    fade: f64,
 }
 
+pub struct Launch {
+    initial_position: Vec3,
+    initial_trajectory: Vec3,
+    initial_normal: Vec3,
+}
+
+pub fn simulate(
+    disc: &Disc,
+    initial_trajectory: &Launch
+) -> Vec<Vec3> {
+    vec![Vec3::new(1.0, 1.0, 1.2)]
+}
