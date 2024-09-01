b56d2e7d8a71633d1d89cbb639a8cdec5cf83aa0
Jeremy Sigrist <jsigrist1@gmail.com>
2024-09-01 00:32:06 -0700

    Initial simple command line interface

--- crafting_interpreters/part1/src/main.rs
+++ crafting_interpreters/part1/src/main.rs
@@ -1,3 +1,29 @@
+use std::path::Path;
+use std::process::exit;
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 2 {
+        eprintln!("Usage: {} [arg]", env::args().next().unwrap());
+        exit(64);
+    } else if args.len() == 2 {
+        println!("Loading {}", args[0]);
+        if let Err(e) = run_file(Path::new(&args[1])) {
+            eprintln!("Failed to read {}: {}", args[1], e);
+            exit(64);
+        }
+    } else {
+        run_prompt();
+    }
 }
+
+fn run_prompt() {}
+
+fn run_file(path: &Path) -> std::io::Result<()> {
+    let contents = fs::read_to_string(path)?;
+    Ok(run(&contents))
+}
+
+fn run(source: &str) {}
